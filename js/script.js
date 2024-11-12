48b08344b11801b2146497308f994e437553af2d
Dawa <atayewdawlet@gmail.com>
2024-11-13 02:05:26 +0500

    adaptive that site

--- js/script.js
+++ js/script.js
@@ -23,11 +23,31 @@ mainLink.addEventListener('click', (e) => {
     copyText();
 });
 
+const burger = document.querySelector('.header__burger');
+const headerNav = document.querySelector('.header__nav');
+
+function openNav(){
+    if (headerNav.classList.contains('active__burger')){
+        headerNav.classList.remove('active__burger');
+    }else{
+        headerNav.classList.add('active__burger');
+    }
+}
+burger.addEventListener('click', openNav);
+
+const headerItem = document.querySelectorAll('.header__item');
+headerItem.forEach(i => {
+    i.parentElement.addEventListener('click', (e) => {
+        headerNav.classList.remove('active__burger');
+    })
+})
+headerItem
+
 //Jupiter Terminal
 window.Jupiter.init({
     displayMode: "integrated",
     integratedTargetId: "integrated-terminal",
-    endpoint: "https://api.mainnet-beta.solana.com",
+    endpoint: "https://api.devnet.solana.com", //https://api.mainnet-beta.solana.com
     formProps: {
         fixedOutputMint: true,
         swapMode: "ExactIn",
